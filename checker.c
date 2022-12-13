92135707d7ed7c928bd640f8b3e77add74da2ae3
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-12-13 20:24:10 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <assert.h>
 
-int IsTemperatureOK(float temperature)
+#define RangeCheck(X,Y,Z) (X<=Y || X>=Z)?0:1
+
+int Check_ChargeRate(float chargeRate);
+
+int batteryIsOk(float temperature, float soc, float chargeRate)
 {
-  (((temperature) < 0) || ((temperature) > 45)) ? 0 : 1;
+  int batterystate = 1;
+  batterystate =  RangeCheck(temperature,0 , 45);
+  batterystate &= RangeCheck(soc,20, 80);
+  batterystate &= Check_ChargeRate(chargeRate);
+  return batterystate;
 }
 
-int IsSocOK(float soc)
+int Check_ChargeRate(float chargeRate)
 {
-  (((soc) < 20) || ((soc) > 80)) ? 0 : 1;
+   if(chargeRate>0.8)
+   return 0;
+   else
+   return 1;
 }
 
-int IsChargeRate(float chargeRate)
-{
-  (chargeRate > 0.8) ? (0):(1);
-}
 
-int batteryIsOk(float temperature, float soc, float chargeRate) 
+int main()
 {
-  int status = 1;
-  
-  status = IsTemperatureOK(temperature);
-  status = IsSocOK(soc);
-  status = IsChargeRate(chargeRate);
-  
-  return status;
-}
-
-int main() {
+ 
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
+
 }
