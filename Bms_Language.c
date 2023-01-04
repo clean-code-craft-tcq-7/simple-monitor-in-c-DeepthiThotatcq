d157a41731cdedc73773bfca839a3d73b468665b
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2023-01-04 23:15:27 +0530

    Update and rename checker.c to Bms_Language.c

---
+++ Bms_Language.c
@@ -0,0 +1,28 @@
+#include "BatteryManagmentSystem.h"
+
+const char* Store_WarningMessage[MESSAGE_MAX];
+const char* WarningMessageTable[MAX_LANG][MESSAGE_MAX] = {
+		{/*DEFAULT */
+			"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"},
+    {/*GERMAN */
+			"LOW_SOC_BREACH","LOW_SOC_WARNUNG","SOC_NORMAL","HIGH_SOC_WARNUNG","HIGH_SOC_BREACH","SOC_UNDEFINED"},
+    { /*CHINA */
+			"L_S_BREACHEN","L_S_WARN!!","NORMAL","H_S_WARN!!","H_S_BREACHEN","UNDEFINED"},
+		{/*ENGLISH */
+			"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"}		
+	};
+
+void SelectLanguageandWarnigMessage(Select_Language Language)
+{   
+   if(Language < MAX_LANG)
+   {
+      for(int i =0;i<MESSAGE_MAX;i++)
+      {
+      	Store_WarningMessage[i] = WarningMessageTable[Language][i];
+      }
+    } 
+}
+const char* ReturnWarningMessage(int soc)
+{
+   return Store_WarningMessage[(BatteryHelathMonitor(soc))];
+}
