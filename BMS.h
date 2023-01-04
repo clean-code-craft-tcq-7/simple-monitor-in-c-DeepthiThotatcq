15158d4c1e0e97e6a62340ddf95d21f85268430a
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2023-01-04 23:58:08 +0530

    Update BMS.h

--- BMS.h
+++ BMS.h
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 typedef enum {
+    DEFAULT,
+    ENGLISH,
+    GERMAN,
+    MAX_LANG
+}Select_Language;
+
+typedef enum {
     LOW_SOC_BREACH,
     LOW_SOC_WARNING,
     SOC_NORMAL,
@@ -11,18 +18,11 @@ typedef enum {
 } WarningRanges;
 
 typedef enum {
-    DEFAULT,
-    ENGLISH,
-    GERMAN,
-	MAX_LANG
-}Select_Language;
-
-typedef enum {
     Approaching_Discharge,
     Approaching_Peak,
     Warning_max
 } WarningWithTolerance;
 
-void SelectLanguageandWarnigMessage(Select_Language Language);
+void SelectLanguageandWarnigMessage(Select_Language);
 WarningRanges BatteryHelathMonitor(int soc);
 const char* ReturnWarningMessage(int soc);
